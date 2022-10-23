7d100e184103b0c5b4ce1261cc4526af285081a3
JakobsonN <jakub.krajniewski@microsoft.wsei.edu.pl>
2022-10-23 14:06:43 +0100

    sdf

--- Intel8086/Program.cs
+++ Intel8086/Program.cs
@@ -2,9 +2,37 @@
 {
     internal class Program
     {
+
+        static void MOV()
+        {
+            data[a] = data[b]
+        }
+
+        static void XCHG()
+        {
+
+
+
+
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] name = {"AL", "AH", "BL", "BH", "CL", "CH", "DL", "DH"};
+            string[] data = new string[8];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine("Podaj zawartosc rejestru " + name[i] +": ");
+                data[i] = Console.ReadLine();
+            }
+
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine("Rejesrt " + name[i] + " miesci "+data[i]);
+            }
         }
     }
 }

8815f147949e88b3448ad0ad08003b8c87c98405
JakobsonN77 <jakubkrajniewski@microsoft.wsei.edu.pl>
2022-10-24 18:28:33 +0200

    tak?

--- Intel8086/Program.cs
+++ Intel8086/Program.cs
@@ -1,22 +1,18 @@
-﻿namespace Intel8086
+﻿using System;
+using System.Globalization;
+
+namespace Intel8086
 {
     internal class Program
     {
 
-        static void MOV(int[] data)
+        static void printRej(string[] data, string[] name)
         {
-            data[1] = data[2];
-
-
-
-        }
-
-        static void XCHG()
-        {
-
-
-
-
+            for (int i = 0; i < data.Length; i++)
+            {
+                int j = i + 1;
+                Console.WriteLine("Rejestr " + j + " " + name[i] + " miesci " + data[i]);
+            }
         }
 
 
@@ -24,51 +20,58 @@
         {
             string[] name = {"AL", "AH", "BL", "BH", "CL", "CH", "DL", "DH"};
             string[] data = new string[8];
-            int a, b;
 
             for (int i = 0; i < data.Length; i++)
             {
                 Console.WriteLine("Podaj zawartosc rejestru " + name[i] +": ");
                 data[i] = Console.ReadLine();
-            }            
-
+            }
+            action:
             Console.WriteLine("co chcesz zrobic?");
             Console.WriteLine("skopiowac to 1");
             Console.WriteLine("wymienić to 2");
             int opt = int.Parse(Console.ReadLine());
 
+            Console.Clear();
             Console.WriteLine("z jakimi rejestrami chcesz pracowac?");
-            for (int i = 0; i < data.Length; i++)
-            {
-                Console.WriteLine("Rejestr " + " " + i + " " + name[i] + " miesci " + data[i]);
-            }
+            printRej(data, name);
 
+            int st = int.Parse(Console.ReadLine());
+            --st;
+            int nd = int.Parse(Console.ReadLine());
+            --nd;
 
             switch (opt)
             {
                 case 1:
-                    MOV(data[a], data[b]);
+                    data[st] = data[nd];
+                    Console.Clear();
+                    printRej(data, name);
                     break;
                 case 2:
-                    string temp;
-                    temp = a;
-                    a = b;
-                    b = temp;
+                    string temp = data[st];
+                    data[st] = data[nd];
+                    data[nd] = temp;
+                    Console.Clear();
                     printRej(data, name);
                     break;
                 default:
                     Console.WriteLine("nie ma takiej mozliwosci");
-                    break;
+                    return ;
             }
-
-
-
-
-
-
-
-
-
+            Console.WriteLine("chcesz przeprowadzic jescze jakas operacje?");
+            Console.WriteLine("T/N");
+            string odp = Console.ReadLine();
+            string odpd = odp.ToUpper(new CultureInfo("en-US", false));
+            if (odpd=="T")
+            {
+                goto action;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("to koniec");
+            }
 
         }
     }

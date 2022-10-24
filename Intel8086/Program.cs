768524a29486ce7442d30131c4eeabb91188e360
JakobsonN77 <jakubkrajniewski@microsoft.wsei.edu.pl>
2022-10-24 17:18:31 +0200

    ?

--- Intel8086/Program.cs
+++ Intel8086/Program.cs
@@ -3,9 +3,12 @@
     internal class Program
     {
 
-        static void MOV()
+        static void MOV(int[] data)
         {
-            data[a] = data[b]
+            data[1] = data[2];
+
+
+
         }
 
         static void XCHG()
@@ -21,18 +24,48 @@
         {
             string[] name = {"AL", "AH", "BL", "BH", "CL", "CH", "DL", "DH"};
             string[] data = new string[8];
+            int a, b;
 
             for (int i = 0; i < data.Length; i++)
             {
                 Console.WriteLine("Podaj zawartosc rejestru " + name[i] +": ");
                 data[i] = Console.ReadLine();
-            }
+            }            
 
+            Console.WriteLine("co chcesz zrobic?");
+            Console.WriteLine("skopiowac to 1");
+            Console.WriteLine("wymienić to 2");
+            int opt = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("z jakimi rejestrami chcesz pracowac?");
             for (int i = 0; i < data.Length; i++)
             {
-                Console.WriteLine("Rejesrt " + name[i] + " miesci "+data[i]);
+                Console.WriteLine("Rejestr " + " " + i + " " + name[i] + " miesci " + data[i]);
             }
+
+
+            switch (opt)
+            {
+                case 1:
+                    MOV(data[a], data[b]);
+                    break;
+                case 2:
+
+                    break;
+                default:
+                    Console.WriteLine("nie ma takiej mozliwosci");
+                    break;
+            }
+
+
+
+
+
+
+
+
+
+
         }
     }
 }

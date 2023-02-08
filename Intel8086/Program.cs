6315d577e3abbc8ff388765f8078c298d11f0d13
JakobsonN77 <jakubkrajniewski@microsoft.wsei.edu.pl>
2023-02-08 22:10:35 +0100

    aaaa

--- Intel8086/Program.cs
+++ Intel8086/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Intel8086
@@ -16,11 +17,20 @@ namespace Intel8086
             }
         }
 
+        static void selRej()
+        {
+            int st = int.Parse(Console.ReadLine());
+            --st;
+            int nd = int.Parse(Console.ReadLine());
+            --nd;
+        }
+
 
         static void Main(string[] args)
         {
             string[] name = {"AL", "AH", "BL", "BH", "CL", "CH", "DL", "DH"};
             int[] data = new int[8];
+            string[] var = {"MOV", "XNG", "NOT", "INC", "DEC", "OR", "AND", "XOR", "ADD", "SUB"}
 
             for (int i = 0; i < data.Length; i++)
             {               
@@ -32,34 +42,78 @@ namespace Intel8086
                     data[i] = int.Parse(Console.ReadLine());
                 }
             }
+            Console.Clear();
             action:
-            Console.WriteLine("co chcesz zrobic?");
-            Console.WriteLine("skopiowac to 1");
-            Console.WriteLine("wymienić to 2");
+
+            Console.WriteLine("Wybierz mozliwa opcje");
+            for (int i =0; i < var.Length; i++) 
+            {
+                int j = i + 1;
+                Console.WriteLine(j + ". " + var[i]);
+            }
+            Console.WriteLine("11. Wyjscie z programu");
             int opt = int.Parse(Console.ReadLine());
 
             Console.Clear();
             Console.WriteLine("z jakimi rejestrami chcesz pracowac?");
-            printRej(data, name);
-
-            int st = int.Parse(Console.ReadLine());
-            --st;
-            int nd = int.Parse(Console.ReadLine());
-            --nd;
 
             switch (opt)
             {
                 case 1:
+                    Console.WriteLine("Wybrales opcje MOV");
+                    selRej;
                     data[st] = data[nd];
                     Console.Clear();
                     printRej(data, name);
                     break;
                 case 2:
+                    Console.WriteLine("Wybrales opcje XNG");
+                    selRej;
                     int temp = data[st];
                     data[st] = data[nd];
                     data[nd] = temp;
                     Console.Clear();
                     printRej(data, name);
+                    break;
+                case 3:
+                    Console.WriteLine("Wybrales opcje NOT");
+                    break;
+                case 4:
+                    Console.WriteLine("Wybrales opcje INC");
+                    break;
+                case 5:
+                    Console.WriteLine("Wybrales opcje DEC");
+                    break; 
+                case 6:
+                    Console.WriteLine("Wybrales opcje OR");
+                    break; 
+                case 7:
+                    Console.WriteLine("Wybrales opcje AND");
+                    break; 
+                case 8:
+                    Console.WriteLine("Wybrales opcje XOR");
+                    break;
+                case 9:
+                    Console.WriteLine("Wybrales opcje ADD");
+                    break;
+                case 10:
+                    Console.WriteLine("Wybrales opcje SUB");
+                    break; 
+                case 11:
+                    Console.WriteLine("Wybrales opcje Wyjscie z programu");
+                    Console.WriteLine("Czy napewno?");
+                    Console.WriteLine("T/N");
+                    string wyjscie = Console.ReadLine()
+                    string wyjscied = wyjscie.ToUpper(new CultureInfo("en-US", false));
+                    if (odpd == "T")
+                    {
+                        goto action;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("to koniec");
+                    }
                     break;
                 default:
                     Console.WriteLine("nie ma takiej mozliwosci");
